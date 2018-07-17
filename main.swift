2240e7290a2689d01640d04d21ce790e06643eb9
Ben Leggiero <BenLeggiero@Gmail.com>
2018-07-17 18:01:18 -0400

    Brought unknown language versions into their own conditional. Discovered 3.3.2

--- main.swift
+++ main.swift
@@ -8,19 +8,25 @@
 
 let languageVersion: String
 
-#if swift(>=5.1) && !swift(>=5.1.1)
+#if swift(>=5.1.1)
+    #error("Unknown language version")
+#elseif swift(>=5.1)
     languageVersion = "5.1"
 #elseif swift(>=5.0)
     languageVersion = "5.0"
 #elseif swift(>=4.2)
     languageVersion = "4.2"
-#elseif swift(>=4.1.2) && !swift(>=4.1.3)
+#elseif swift(>=4.1.3)
+    #error("Unknown language version")
+#elseif swift(>=4.1.2)
     languageVersion = "4.1.2"
 #elseif swift(>=4.1.1)
     languageVersion = "4.1.1"
 #elseif swift(>=4.1.0)
     languageVersion = "4.1.0"
-#elseif swift(>=4.0.3) && !swift(>=4.0.4)
+#elseif swift(>=4.0.4)
+    #error("Unknown language version")
+#elseif swift(>=4.0.3)
     languageVersion = "4.0.3"
 #elseif swift(>=4.0.2)
     languageVersion = "4.0.2"
@@ -28,13 +34,25 @@ let languageVersion: String
     languageVersion = "4.0.1"
 #elseif swift(>=4.0)
     languageVersion = "4.0"
-#elseif swift(>=3.4) && !swift(>=3.4.1)
+#elseif swift(>=3.4.1)
+    #error("Unknown language version")
+#elseif swift(>=3.4)
     languageVersion = "3.4"
-#elseif swift(>=3.3) && !swift(>=3.3.1)
+#elseif swift(>=3.3.3)
+    #error("Unknown language version")
+#elseif swift(>=3.3.2)
+    languageVersion = "3.3.2"
+#elseif swift(>=3.3.1)
+    languageVersion = "3.3.1"
+#elseif swift(>=3.3)
     languageVersion = "3.3"
-#elseif swift(>=3.2) && !swift(>=3.2.1)
+#elseif swift(>=3.2.1)
+    #error("Unknown language version")
+#elseif swift(>=3.2)
     languageVersion = "3.2"
-#elseif swift(>=3.1.1) && !swift(>=3.1.2)
+#elseif swift(>=3.1.2)
+    #error("Unknown language version")
+#elseif swift(>=3.1.1)
     languageVersion = "3.1.1"
 #elseif swift(>=3.1)
     languageVersion = "3.1"
